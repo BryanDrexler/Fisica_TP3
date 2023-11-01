b6fabb96166da47ac23dc3ad6fc77a84bf083e36
BryanDrexler <bdrexler@udesa.edu.ar>
2023-10-31 22:55:06 -0300

    .

--- leer.py
+++ leer.py
@@ -1,23 +1,28 @@
 import csv
 import os
+import pandas as pd
 
-def split_csv(filename):
-    with open(filename, 'r') as f:
-        reader = csv.reader(f)
-        file_number = 1
-        current_file = open(f'medicion{file_number}.csv', 'w')
-        writer = csv.writer(current_file)
-        for row in reader:
-            if row and row[0].startswith('#'):
-                if os.fstat(current_file.fileno()).st_size > 0:  # Si el archivo actual no está vacío, entonces crea uno nuevo
-                    current_file.close()
-                    file_number += 1
-                    current_file = open(f'medicion{file_number}.csv', 'w')
-                    writer = csv.writer(current_file)
-            else:
-                writer.writerow(row)
-        current_file.close()
+import csv
 
-# Uso de la función
-split_csv('dataset.csv')
+def read_measures():
+    datos = []
+    for i in range(1, 14):
+        # Construimos el nombre del archivo
+        archivo = f"mediciones/medicion{i}.csv"
+        datos_archivo = []
+
+        with open(archivo, 'r', newline='') as csvfile:
+            csvreader = csv.reader(csvfile)
+            next(csvreader)  # Omitir la primera fila
+
+            for row in csvreader:
+                datos_archivo.append(row)
+
+        datos.append(datos_archivo)
+
+    return datos
+
+datos = read_measures()
+
+print(datos)
 
